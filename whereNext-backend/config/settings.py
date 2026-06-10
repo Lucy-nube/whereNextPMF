770f295dfc770e6c68f86907ced446fef90d79b7
Lucy-nube <Lucy24072001@gmail.com>
2026-06-10 13:43:00 +0200

    adding postgress

--- whereNext-backend/config/settings.py
+++ whereNext-backend/config/settings.py
@@ -124,13 +124,12 @@ TEMPLATES = [
 
 
 
-load_dotenv()
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=False  # True solo en Render
+        ssl_require=not DEBUG,
     )
 }
 # ========================
