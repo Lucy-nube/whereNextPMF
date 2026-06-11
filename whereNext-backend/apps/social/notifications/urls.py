af7c2e13c9af7511ac15745d5b599dfd4768d179
Lucy-nube <Lucy24072001@gmail.com>
2026-06-11 11:25:56 +0200

    adding notifications to admin

--- whereNext-backend/apps/social/notifications/urls.py
+++ whereNext-backend/apps/social/notifications/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import NotificationListView, NotificationDetailView,NotificationMarkAllReadView
+from .views import NotificationListView, NotificationDetailView, NotificationMarkAllReadView
 
 urlpatterns = [
     path("", NotificationListView.as_view(), name="notifications-list"),
     path("<int:pk>/", NotificationDetailView.as_view(), name="notification-detail"),
+    path("<int:pk>/mark_read/", NotificationDetailView.as_view(), name="notification-mark-read"),
     path("read_all/", NotificationMarkAllReadView.as_view()),
-
 ]
